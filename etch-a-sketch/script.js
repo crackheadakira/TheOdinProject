a46350e7c4a798b3feb0d9038eda1569c8d97097
crackheadakira <lasn.mine@gmail.com>
2022-04-11 21:52:08 +0200

    finished etch-a-sketch

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -1,9 +1,32 @@
-function createSquare(height, width) {
-    let box = document.createElement("div");
-    box.width = width;
-    box.height = height;
-    return box;
+const grid = document.querySelector('.grid');
+let currentColor = "black";
+let currentSize = 16
+
+function createGrid(size = 16) {
+    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`
+    for (let i = 0; i < size * size; i++) {
+        const gridElement = document.createElement('div')
+        gridElement.classList = "gridElement"
+        gridElement.addEventListener('mousedown', () => {
+            gridElement.style.backgroundColor = currentColor
+        })
+        grid.appendChild(gridElement)
+    }
 }
 
-let squareGrid = document.querySelector("#squareGrid")
-squareGrid.appendChild(createSquare(16, 16))
+function resetGrid() {
+    currentSize = prompt("What should the new pixel grid size be? Answer in pixels, maximum of 100.")
+    if (currentSize > 100) {
+        currentSize = 100;
+    } else if (currentSize < 1) {
+        currentSize = 16
+    }
+    grid.innerHTML = '';
+    createGrid(currentSize)
+}
+
+let button = document.querySelector('.resetButton')
+button.addEventListener('click', resetGrid)
+
+createGrid(currentSize)
