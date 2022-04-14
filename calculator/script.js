236f98827949ce3092db553b648cd92c4ef9ac68
crackheadakira <lasn.mine@gmail.com>
2022-04-14 03:36:23 +0200

    gonna hop into bed, commiting code so far

--- calculator/script.js
+++ calculator/script.js
@@ -1,3 +1,20 @@
+const buttons = document.querySelectorAll('.digitButton')
+const resultBar = document.querySelector('.resultBar')
+const clearButton = document.querySelector('.clearButton');
+const addButton = document.querySelector('#addButton');
+const equalsButton = document.querySelector('#equalsButton')
+
+let displayResult = [];
+let number1;
+let number2 = 20;
+let finalNumber;
+let operator;
+
+function clear() {
+    displayResult = [];
+    resultBar.textContent = displayResult;
+}
+
 function add(nr1, nr2) {
     return nr1 + nr2
 }
@@ -27,10 +44,34 @@ function operate(operator, nr1, nr2) {
     }
 }
 
-const buttons = document.querySelectorAll('.buttons')
+function display(number) {
+    displayResult = displayResult.concat(number)
+    displayResult = displayResult.toString().replaceAll(',', "")
+    resultBar.textContent = +displayResult;
+}
+
+function calculate(cOperator, nr1, nr2) {
+    cOperator = operator
+    operate(cOperator, nr1,nr2)
+}
+
+equalsButton.addEventListener('click', () => {
+    finalNumber = operate(operator, +number1, +number2);
+    resultBar.textContent = displayResult;
+})
+
+clearButton.addEventListener('click', () => {
+    clear()
+})
+
+addButton.addEventListener('click', () => {
+    number1 = displayResult
+    operator = add;
+    console.log(number1)
+})
 
 buttons.forEach((button) => {
     button.addEventListener('click', (e) => {
-        console.log(e.target.firstChild.data);
+        display(e.target.firstChild.data);
     })
 })
